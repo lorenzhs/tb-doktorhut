b0b60c49f4f409193d1c9187a5a07ea89c0f4fa1
Lorenz Hübschle-Schneider <dev@4z2.de>
2018-06-27 19:00:08 +0200

    save

--- main/main.c
+++ main/main.c
@@ -1,6 +1,9 @@
 #include <string.h>
 
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
 #include "esp_wifi.h"
 #include "esp_system.h"
 #include "esp_event.h"
@@ -8,218 +11,194 @@
 #include "esp_log.h"
 #include "esp_spi_flash.h"
 #include "nvs_flash.h"
+
+//#include "esp_http_client.h"
+
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
+
+/*=================================================*/
+// display stuff
 #include "driver/gpio.h"
 #include "driver/i2c.h"
 
-#include "ssd1366.h"
-#include "font8x8_basic.h"
-
 #define SDA_PIN GPIO_NUM_15
 #define SCL_PIN GPIO_NUM_2
-
 #define tag "SSD1306"
 
-void i2c_master_init()
-{
-	i2c_config_t i2c_config = {
-		.mode = I2C_MODE_MASTER,
-		.sda_io_num = SDA_PIN,
-		.scl_io_num = SCL_PIN,
-		.sda_pullup_en = GPIO_PULLUP_ENABLE,
-		.scl_pullup_en = GPIO_PULLUP_ENABLE,
-		.master.clk_speed = 1000000
-	};
-	i2c_param_config(I2C_NUM_0, &i2c_config);
-	i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
-}
+#include "font8x8_basic.h"
+#include "ssd1366.h"
 
-void ssd1306_init() {
-	esp_err_t espRc;
+/*=================================================*/
+// wifi stuff
 
-	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+#define WIFI_SSID "KA-WLAN"
+#define WIFI_PASS ""
 
-	i2c_master_start(cmd);
-	i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-	i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+/* FreeRTOS event group to signal when we are connected & ready to make a request */
+static EventGroupHandle_t wifi_event_group;
 
-	i2c_master_write_byte(cmd, OLED_CMD_SET_CHARGE_PUMP, true);
-	i2c_master_write_byte(cmd, 0x14, true);
+/* The event group allows multiple bits for each event,
+   but we only care about one event - are we connected
+   to the AP with an IP? */
+const int CONNECTED_BIT = BIT0;
 
-	i2c_master_write_byte(cmd, OLED_CMD_SET_SEGMENT_REMAP, true); // reverse left-right mapping
-	i2c_master_write_byte(cmd, OLED_CMD_SET_COM_SCAN_MODE, true); // reverse up-bottom mapping
+#define WEB_SERVER "webrates.truefx.com"
+#define WEB_PORT "80"
+#define WEB_URL "http://webrates.truefx.com/rates/connect.html"
 
-	i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_ON, true);
-	i2c_master_stop(cmd);
+static const char *TAG = "example";
 
-	espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
-	if (espRc == ESP_OK) {
-		ESP_LOGI(tag, "OLED configured successfully");
-	} else {
-		ESP_LOGE(tag, "OLED configuration failed. code: 0x%.2X", espRc);
-	}
-	i2c_cmd_link_delete(cmd);
-}
-
-void task_ssd1306_display_pattern(void *ignore) {
-	i2c_cmd_handle_t cmd;
-
-	for (uint8_t i = 0; i < 8; i++) {
-		cmd = i2c_cmd_link_create();
-		i2c_master_start(cmd);
-		i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_SINGLE, true);
-		i2c_master_write_byte(cmd, 0xB0 | i, true);
-		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
-		for (uint8_t j = 0; j < 128; j++) {
-			i2c_master_write_byte(cmd, 0xFF >> (j % 8), true);
-		}
-		i2c_master_stop(cmd);
-		i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
-		i2c_cmd_link_delete(cmd);
-	}
-
-	vTaskDelete(NULL);
-}
-
-void task_ssd1306_display_clear(void *ignore) {
-	i2c_cmd_handle_t cmd;
-
-	uint8_t zero[128];
-        memset(zero, 0, sizeof zero);
-	for (uint8_t i = 0; i < 8; i++) {
-		cmd = i2c_cmd_link_create();
-		i2c_master_start(cmd);
-		i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_SINGLE, true);
-		i2c_master_write_byte(cmd, 0xB0 | i, true);
-
-		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
-		i2c_master_write(cmd, zero, 128, true);
-		i2c_master_stop(cmd);
-		i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
-		i2c_cmd_link_delete(cmd);
-	}
-
-	vTaskDelete(NULL);
-}
+static const char *REQUEST = "GET " WEB_URL " HTTP/1.0\r\n"
+    "Host: "WEB_SERVER"\r\n"
+    "User-Agent: esp-idf/1.0 esp32\r\n"
+    "\r\n";
 
 
-void task_ssd1306_contrast(void *ignore) {
-	i2c_cmd_handle_t cmd;
-
-	uint8_t contrast = 0;
-	uint8_t direction = 1;
-	while (true) {
-		cmd = i2c_cmd_link_create();
-		i2c_master_start(cmd);
-		i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
-		i2c_master_write_byte(cmd, OLED_CMD_SET_CONTRAST, true);
-		i2c_master_write_byte(cmd, contrast, true);
-		i2c_master_stop(cmd);
-		i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
-		i2c_cmd_link_delete(cmd);
-		vTaskDelay(1/portTICK_PERIOD_MS);
-
-		contrast += direction;
-		if (contrast == 0xFF) { direction = -1; }
-		if (contrast == 0x0) { direction = 1; }
-	}
-	vTaskDelete(NULL);
-}
-
-void task_ssd1306_scroll(void *ignore) {
-	esp_err_t espRc;
-
-	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-	i2c_master_start(cmd);
-
-	i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-	i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
-
-	i2c_master_write_byte(cmd, 0x29, true); // vertical and horizontal scroll (p29)
-	i2c_master_write_byte(cmd, 0x00, true);
-	i2c_master_write_byte(cmd, 0x00, true);
-	i2c_master_write_byte(cmd, 0x07, true);
-	i2c_master_write_byte(cmd, 0x01, true);
-	i2c_master_write_byte(cmd, 0x3F, true);
-
-	i2c_master_write_byte(cmd, 0xA3, true); // set vertical scroll area (p30)
-	i2c_master_write_byte(cmd, 0x20, true);
-	i2c_master_write_byte(cmd, 0x40, true);
-
-	i2c_master_write_byte(cmd, 0x2F, true); // activate scroll (p29)
-
-	i2c_master_stop(cmd);
-	espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
-	if (espRc == ESP_OK) {
-		ESP_LOGI(tag, "Scroll command succeeded");
-	} else {
-		ESP_LOGE(tag, "Scroll command failed. code: 0x%.2X", espRc);
-	}
-
-	i2c_cmd_link_delete(cmd);
-
-	vTaskDelete(NULL);
-}
-
-void task_ssd1306_display_text(const void *arg_text) {
-	char *text = (char*)arg_text;
-	uint8_t text_len = strlen(text);
-
-	i2c_cmd_handle_t cmd;
-
-	uint8_t cur_page = 0;
-
-	cmd = i2c_cmd_link_create();
-	i2c_master_start(cmd);
-	i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-
-	i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
-	i2c_master_write_byte(cmd, 0x00, true); // reset column
-	i2c_master_write_byte(cmd, 0x10, true);
-	i2c_master_write_byte(cmd, 0xB0 | cur_page, true); // reset page
-
-	i2c_master_stop(cmd);
-	i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
-	i2c_cmd_link_delete(cmd);
-
-	for (uint8_t i = 0; i < text_len; i++) {
-		if (text[i] == '\n') {
-			cmd = i2c_cmd_link_create();
-			i2c_master_start(cmd);
-			i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-
-			i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
-			i2c_master_write_byte(cmd, 0x00, true); // reset column
-			i2c_master_write_byte(cmd, 0x10, true);
-			i2c_master_write_byte(cmd, 0xB0 | ++cur_page, true); // increment page
-
-			i2c_master_stop(cmd);
-			i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
-			i2c_cmd_link_delete(cmd);
-		} else {
-			cmd = i2c_cmd_link_create();
-			i2c_master_start(cmd);
-			i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-
-			i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
-			i2c_master_write(cmd, font8x8_basic_tr[(uint8_t)text[i]], 8, true);
-
-			i2c_master_stop(cmd);
-			i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
-			i2c_cmd_link_delete(cmd);
-		}
-	}
-
-	vTaskDelete(NULL);
-}
-
+/*=================================================*/
+/*============ WiFi helper functions ==============*/
+/*=================================================*/
 
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
+    switch(event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+        esp_wifi_connect();
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+        xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        /* This is a workaround as ESP32 WiFi libs don't currently
+           auto-reassociate. */
+        esp_wifi_connect();
+        xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+        break;
+    default:
+        break;
+    }
     return ESP_OK;
 }
+
+static void initialise_wifi(void)
+{
+    tcpip_adapter_init();
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+        },
+    };
+    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+}
+
+/*=================================================*/
+
+
+static void http_get_task(char* server, char* port, char* request,
+                          void *pvParameters)
+{
+    const struct addrinfo hints = {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+    };
+    struct addrinfo *res;
+    struct in_addr *addr;
+    int s, r;
+    char recv_buf[64];
+
+    while(1) {
+        /* Wait for the callback to set the CONNECTED_BIT in the
+           event group.
+        */
+        xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
+                            false, true, portMAX_DELAY);
+        ESP_LOGI(TAG, "Connected to AP");
+
+        int err = getaddrinfo(server, port, &hints, &res);
+
+        if(err != 0 || res == NULL) {
+            ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        /* Code to print the resolved IP.
+
+           Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code */
+        addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
+        ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
+
+        s = socket(res->ai_family, res->ai_socktype, 0);
+        if(s < 0) {
+            ESP_LOGE(TAG, "... Failed to allocate socket.");
+            freeaddrinfo(res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... allocated socket");
+
+        if(connect(s, res->ai_addr, res->ai_addrlen) != 0) {
+            ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
+            close(s);
+            freeaddrinfo(res);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        ESP_LOGI(TAG, "... connected");
+        freeaddrinfo(res);
+
+        if (write(s, request, strlen(request)) < 0) {
+            ESP_LOGE(TAG, "... socket send failed");
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... socket send success");
+
+        struct timeval receiving_timeout;
+        receiving_timeout.tv_sec = 5;
+        receiving_timeout.tv_usec = 0;
+        if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
+                sizeof(receiving_timeout)) < 0) {
+            ESP_LOGE(TAG, "... failed to set socket receiving timeout");
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... set socket receiving timeout success");
+
+        /* Read HTTP response */
+        do {
+            bzero(recv_buf, sizeof(recv_buf));
+            r = read(s, recv_buf, sizeof(recv_buf)-1);
+            for(int i = 0; i < r; i++) {
+                putchar(recv_buf[i]);
+            }
+        } while(r > 0);
+
+        ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d\r\n", r, errno);
+        close(s);
+        for(int countdown = 10; countdown >= 0; countdown--) {
+            ESP_LOGI(TAG, "%d... ", countdown);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+        ESP_LOGI(TAG, "Starting again!");
+    }
+}
+
 
 void app_main(void)
 {
@@ -236,6 +215,7 @@ void app_main(void)
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
+/* init display */
     i2c_master_init();
     ssd1306_init();
 
@@ -243,9 +223,10 @@ void app_main(void)
     vTaskDelay(100/portTICK_PERIOD_MS);
     xTaskCreate(&task_ssd1306_display_text, "ssd1306_display_text", 2048,
                 (void *)"EUR/USD=1.2345", 6, NULL);
+/* end display init */
 
-
-    nvs_flash_init();
+    ESP_ERROR_CHECK( nvs_flash_init() );
+    /*
     tcpip_adapter_init();
     ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -269,5 +250,9 @@ void app_main(void)
         gpio_set_level(GPIO_NUM_4, level);
         level = !level;
         vTaskDelay(300 / portTICK_PERIOD_MS);
-    }
+        }*/
+
+    initialise_wifi();
+    /*
+       xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL); */
 }
--- main/ssd1366.h
+++ main/ssd1366.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_SSD1366_H_
 #define MAIN_SSD1366_H_
 
-// Following definitions are bollowed from 
+// Following definitions are bollowed from
 // http://robotcantalk.blogspot.com/2015/03/interfacing-arduino-with-ssd1306-driven.html
 
 // SLA (0x3C) + WRITE_MODE (0x00) =  0x78 (0b01111000)
@@ -28,9 +28,9 @@
 
 // Hardware Config (pg.31)
 #define OLED_CMD_SET_DISPLAY_START_LINE 0x40
-#define OLED_CMD_SET_SEGMENT_REMAP      0xA1    
+#define OLED_CMD_SET_SEGMENT_REMAP      0xA1
 #define OLED_CMD_SET_MUX_RATIO          0xA8    // follow with 0x3F = 64 MUX
-#define OLED_CMD_SET_COM_SCAN_MODE      0xC8    
+#define OLED_CMD_SET_COM_SCAN_MODE      0xC8
 #define OLED_CMD_SET_DISPLAY_OFFSET     0xD3    // follow with 0x00
 #define OLED_CMD_SET_COM_PIN_MAP        0xDA    // follow with 0x12
 #define OLED_CMD_NOP                    0xE3    // NOP
@@ -42,5 +42,202 @@
 
 // Charge Pump (pg.62)
 #define OLED_CMD_SET_CHARGE_PUMP        0x8D    // follow with 0x14
+
+
+void i2c_master_init()
+{
+    i2c_config_t i2c_config = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = SDA_PIN,
+        .scl_io_num = SCL_PIN,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = 1000000
+    };
+    i2c_param_config(I2C_NUM_0, &i2c_config);
+    i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
+}
+
+void ssd1306_init() {
+    esp_err_t espRc;
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+
+    i2c_master_write_byte(cmd, OLED_CMD_SET_CHARGE_PUMP, true);
+    i2c_master_write_byte(cmd, 0x14, true);
+
+    i2c_master_write_byte(cmd, OLED_CMD_SET_SEGMENT_REMAP, true); // reverse left-right mapping
+    i2c_master_write_byte(cmd, OLED_CMD_SET_COM_SCAN_MODE, true); // reverse up-bottom mapping
+
+    i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_ON, true);
+    i2c_master_stop(cmd);
+
+    espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+    if (espRc == ESP_OK) {
+        ESP_LOGI(tag, "OLED configured successfully");
+    } else {
+        ESP_LOGE(tag, "OLED configuration failed. code: 0x%.2X", espRc);
+    }
+    i2c_cmd_link_delete(cmd);
+}
+
+void task_ssd1306_display_pattern(void *ignore) {
+    i2c_cmd_handle_t cmd;
+
+    for (uint8_t i = 0; i < 8; i++) {
+        cmd = i2c_cmd_link_create();
+        i2c_master_start(cmd);
+        i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+        i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_SINGLE, true);
+        i2c_master_write_byte(cmd, 0xB0 | i, true);
+        i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+        for (uint8_t j = 0; j < 128; j++) {
+            i2c_master_write_byte(cmd, 0xFF >> (j % 8), true);
+        }
+        i2c_master_stop(cmd);
+        i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+        i2c_cmd_link_delete(cmd);
+    }
+
+    vTaskDelete(NULL);
+}
+
+void task_ssd1306_display_clear(void *ignore) {
+    i2c_cmd_handle_t cmd;
+
+    uint8_t zero[128];
+    memset(zero, 0, sizeof zero);
+    for (uint8_t i = 0; i < 8; i++) {
+        cmd = i2c_cmd_link_create();
+        i2c_master_start(cmd);
+        i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+        i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_SINGLE, true);
+        i2c_master_write_byte(cmd, 0xB0 | i, true);
+
+        i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+        i2c_master_write(cmd, zero, 128, true);
+        i2c_master_stop(cmd);
+        i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+        i2c_cmd_link_delete(cmd);
+    }
+
+    vTaskDelete(NULL);
+}
+
+
+void task_ssd1306_contrast(void *ignore) {
+    i2c_cmd_handle_t cmd;
+
+    uint8_t contrast = 0;
+    uint8_t direction = 1;
+    while (true) {
+        cmd = i2c_cmd_link_create();
+        i2c_master_start(cmd);
+        i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+        i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+        i2c_master_write_byte(cmd, OLED_CMD_SET_CONTRAST, true);
+        i2c_master_write_byte(cmd, contrast, true);
+        i2c_master_stop(cmd);
+        i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+        i2c_cmd_link_delete(cmd);
+        vTaskDelay(1/portTICK_PERIOD_MS);
+
+        contrast += direction;
+        if (contrast == 0xFF) { direction = -1; }
+        if (contrast == 0x0) { direction = 1; }
+    }
+    vTaskDelete(NULL);
+}
+
+void task_ssd1306_scroll(void *ignore) {
+    esp_err_t espRc;
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+
+    i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+
+    i2c_master_write_byte(cmd, 0x29, true); // vertical and horizontal scroll (p29)
+    i2c_master_write_byte(cmd, 0x00, true);
+    i2c_master_write_byte(cmd, 0x00, true);
+    i2c_master_write_byte(cmd, 0x07, true);
+    i2c_master_write_byte(cmd, 0x01, true);
+    i2c_master_write_byte(cmd, 0x3F, true);
+
+    i2c_master_write_byte(cmd, 0xA3, true); // set vertical scroll area (p30)
+    i2c_master_write_byte(cmd, 0x20, true);
+    i2c_master_write_byte(cmd, 0x40, true);
+
+    i2c_master_write_byte(cmd, 0x2F, true); // activate scroll (p29)
+
+    i2c_master_stop(cmd);
+    espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+    if (espRc == ESP_OK) {
+        ESP_LOGI(tag, "Scroll command succeeded");
+    } else {
+        ESP_LOGE(tag, "Scroll command failed. code: 0x%.2X", espRc);
+    }
+
+    i2c_cmd_link_delete(cmd);
+
+    vTaskDelete(NULL);
+}
+
+void task_ssd1306_display_text(const void *arg_text) {
+    char *text = (char*)arg_text;
+    uint8_t text_len = strlen(text);
+
+    i2c_cmd_handle_t cmd;
+
+    uint8_t cur_page = 0;
+
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+    i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+    i2c_master_write_byte(cmd, 0x00, true); // reset column
+    i2c_master_write_byte(cmd, 0x10, true);
+    i2c_master_write_byte(cmd, 0xB0 | cur_page, true); // reset page
+
+    i2c_master_stop(cmd);
+    i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+
+    for (uint8_t i = 0; i < text_len; i++) {
+        if (text[i] == '\n') {
+            cmd = i2c_cmd_link_create();
+            i2c_master_start(cmd);
+            i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+            i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+            i2c_master_write_byte(cmd, 0x00, true); // reset column
+            i2c_master_write_byte(cmd, 0x10, true);
+            i2c_master_write_byte(cmd, 0xB0 | ++cur_page, true); // increment page
+
+            i2c_master_stop(cmd);
+            i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+            i2c_cmd_link_delete(cmd);
+        } else {
+            cmd = i2c_cmd_link_create();
+            i2c_master_start(cmd);
+            i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+            i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+            i2c_master_write(cmd, font8x8_basic_tr[(uint8_t)text[i]], 8, true);
+
+            i2c_master_stop(cmd);
+            i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+            i2c_cmd_link_delete(cmd);
+        }
+    }
+
+    vTaskDelete(NULL);
+}
 
 #endif /* MAIN_SSD1366_H_ */

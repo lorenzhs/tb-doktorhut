b13d1114584e1ce41bc7b8d8297da418164e2706
Lorenz Hübschle-Schneider <dev@4z2.de>
2018-06-29 12:53:22 +0200

    1 led

--- main/main.c
+++ main/main.c
@@ -43,6 +43,96 @@ const int CONNECTED_BIT = BIT0;
 static const char *TAG = "tbhut";
 
 
+/*=================================================*/
+// LED stuff
+//#include "ws2812.h"
+
+//#define WS2812_PIN 18
+//#define delay_ms(ms) vTaskDelay((ms) / portTICK_RATE_MS)
+
+#include "driver/rmt.h"
+
+static const char *RMT_TX_TAG = "RMT Tx";
+
+#define RMT_TX_CHANNEL RMT_CHANNEL_0
+#define RMT_TX_GPIO 18
+
+rmt_item32_t pixels[] = {
+
+   // 8 Bit G
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+
+   // 8 Bit R
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+
+   // 8 Bit B
+   {{{6, 1, 6, 0}}},
+   {{{6, 1, 6, 0}}},
+   {{{6, 1, 6, 0}}},
+   {{{6, 1, 6, 0}}},
+   {{{6, 1, 6, 0}}},
+   {{{6, 1, 6, 0}}},
+   {{{6, 1, 6, 0}}},
+   {{{6, 1, 6, 0}}},
+
+   // 8 Bit W
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+   {{{3, 1, 9, 0}}},
+
+   {{{0, 0, 800, 0}}},
+
+   // RMT end marker
+   {{{ 0, 0, 0, 0 }}}
+};
+
+
+/*
+ * Initialize the RMT Tx channel
+ */
+static void rmt_tx_int()
+{
+    rmt_config_t config;
+    config.rmt_mode = RMT_MODE_TX;
+    config.channel = RMT_TX_CHANNEL;
+    config.gpio_num = RMT_TX_GPIO;
+    config.mem_block_num = 1;
+    config.tx_config.loop_en = 0;
+    config.tx_config.carrier_en = 0;
+    config.tx_config.idle_output_en = 1;
+    config.tx_config.idle_level = (rmt_idle_level_t)0;
+    config.tx_config.carrier_duty_percent = 50;
+    config.tx_config.carrier_freq_hz = 10000;
+    config.tx_config.carrier_level = (rmt_carrier_level_t)0;
+    // set the maximum clock divider to be able to output
+    // RMT pulses in range of about one hundred nanoseconds
+    // a divider of 8 will give us 80 MHz / 8 = 10 MHz -> 0.1 us
+    config.clk_div = 8;
+
+    ESP_ERROR_CHECK(rmt_config(&config));
+    ESP_ERROR_CHECK(rmt_driver_install(config.channel, 0, 0));
+}
+
+
 
 /******************************************************************************/
 /*** WiFi Login (KA-WLAN)******************************************************/
@@ -286,6 +376,79 @@ static void initialise_wifi(void)
 }
 
 /******************************************************************************/
+/*** LED control **************************************************************/
+/*
+
+void rainbow(void *pvParameters)
+{
+  const uint8_t anim_step = 10;
+  const uint8_t anim_max = 250;
+  const uint8_t pixel_count = 32; // Number of your "pixels"
+  const uint8_t delay = 25; // duration between color changes
+  rgbVal color = makeRGBVal(anim_max, 0, 0);
+  uint8_t step = 0;
+  rgbVal color2 = makeRGBVal(anim_max, 0, 0);
+  uint8_t step2 = 0;
+  rgbVal *pixels;
+
+
+  pixels = malloc(sizeof(rgbVal) * pixel_count);
+
+  while (1) {
+    color = color2;
+    step = step2;
+
+    for (uint8_t i = 0; i < pixel_count; i++) {
+      pixels[i] = color;
+
+      if (i == 1) {
+        color2 = color;
+        step2 = step;
+      }
+
+      switch (step) {
+      case 0:
+        color.g += anim_step;
+        if (color.g >= anim_max)
+          step++;
+        break;
+      case 1:
+        color.r -= anim_step;
+        if (color.r == 0)
+          step++;
+        break;
+      case 2:
+        color.b += anim_step;
+        if (color.b >= anim_max)
+          step++;
+        break;
+      case 3:
+        color.g -= anim_step;
+        if (color.g == 0)
+          step++;
+        break;
+      case 4:
+        color.r += anim_step;
+        if (color.r >= anim_max)
+          step++;
+        break;
+      case 5:
+        color.b -= anim_step;
+        if (color.b == 0)
+          step = 0;
+        break;
+      }
+    }
+
+    ws2812_setColors(pixel_count, pixels);
+
+    delay_ms(delay);
+  }
+}
+
+*/
+
+/******************************************************************************/
 /*** Main Logic ***************************************************************/
 
 void app_main(void)
@@ -314,6 +477,21 @@ void app_main(void)
 /* end display init */
 
     ESP_ERROR_CHECK( nvs_flash_init() );
+
+    // Rainbow LEDs!
+
+    ESP_LOGI(RMT_TX_TAG, "Configuring transmitter");
+    rmt_tx_int();
+    int number_of_items = sizeof(pixels) / sizeof(pixels[0]);
+
+    while (1) {
+        ESP_ERROR_CHECK(rmt_write_items(RMT_TX_CHANNEL, pixels, number_of_items, 1));
+        ESP_LOGI(RMT_TX_TAG, "Transmission complete");
+        //vTaskDelay(6000 / portTICK_PERIOD_MS);
+    }
+
+    //ws2812_init(WS2812_PIN);
+    //xTaskCreate(rainbow, "ws2812 rainbow demo", 4096, NULL, 10, NULL);
 
     // Go for WiFi!
     initialise_wifi();

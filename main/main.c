1ac7e7d726d24386270b2b38d005206fc348c824
Lorenz Hübschle-Schneider <dev@4z2.de>
2018-07-02 13:26:53 +0200

    add basic led stuff

--- main/main.c
+++ main/main.c
@@ -52,6 +52,42 @@ const int CONNECTED_BIT = BIT0;
 static const char *TAG = "tbhut";
 
 
+/***************************************************/
+// LED stuff
+
+#include "esp32_digital_led_lib.h"
+
+#define LED_PIN GPIO_NUM_14
+
+#ifndef __cplusplus
+#define nullptr  NULL
+#endif
+
+#define HIGH 1
+#define LOW 0
+/*
+#define DEC 10
+#define HEX 16
+#define OCT 8
+#define BIN 2
+*/
+
+#define min(a, b)  ((a) < (b) ? (a) : (b))
+#define max(a, b)  ((a) > (b) ? (a) : (b))
+#define floor(a)   ((int)(a))
+#define ceil(a)    ((int)((int)(a) < (a) ? (a+1) : (a)))
+
+
+strand_t STRANDS[] = { // Avoid using any of the strapping pins on the ESP32
+    {.rmtChannel = 1, .gpioNum = LED_PIN, .ledType = LED_SK6812W_V1,
+     .brightLimit = 32, .numPixels =  41, .pixels = nullptr,
+     ._stateVars = nullptr},
+};
+
+int STRANDCNT = sizeof(STRANDS)/sizeof(STRANDS[0]);
+
+
+
 /******************************************************************************/
 /*** WiFi Login (KA-WLAN)******************************************************/
 
@@ -309,6 +345,136 @@ static void initialise_wifi(void)
 }
 
 
+
+/******************************************************************************/
+/*** LED control***************************************************************/
+
+void LED_setup(gpio_num_t gpioNum, int gpioMode, int gpioVal) {
+    gpio_mode_t gpioModeNative = (gpio_mode_t)(gpioMode);
+    gpio_pad_select_gpio(gpioNum);
+    gpio_set_direction(gpioNum, gpioModeNative);
+    gpio_set_level(gpioNum, gpioVal);
+}
+
+uint32_t IRAM_ATTR millis()
+{
+  return xTaskGetTickCount() * portTICK_PERIOD_MS;
+}
+
+pixelColor_t HSV_to_RGB( float h, float s, float v ) {
+    /* h in in range [0,6], s and v in [0,1]
+       result is in form #rrggbb */
+    int i;
+    float m, n, f;
+
+    // Saturation or value of out range => return black
+    if ((s<0.0) || (s>1.0) || (v<0.0) || (v>1.0)) {
+        return 0L;
+    }
+
+    if ((h < 0.0) || (h > 6.0)) {
+        // Hue out of range => return grey according to value
+        v *= 255;
+        return long(v) | long(v) << 8 | long(v) << 16;
+    }
+
+    i = floor(h);
+    f = h - i;
+    if ( !(i&1) ) {
+        f = 1 - f; // if i is even
+    }
+    m = v * (1 - s);
+    n = v * (1 - s * f);
+
+    v *= 255;
+    n *= 255;
+    m *= 255;
+    /* B, R, G */
+    switch (i) {
+    case 6:
+    case 0: // (v, n, m)
+        return pixelFromRGB(n, m, v);
+        //return long(v ) << 16 | long(n) << 8 | long(m);
+    case 1: // (n, v, m)
+        return pixelFromRGB(v, m, n);
+        //return long(n ) << 16 | long(v) << 8 | long(m);
+    case 2: // (m, v, n)
+        return pixelFromRGB(v, n, m);
+        //return long(m ) << 16 | long(v) << 8 | long(n);
+    case 3: // (m, n, v)
+        return pixelFromRGB(n, v, m);
+        //return long(m ) << 16 | long(n) << 8 | long(v);
+    case 4: // (n, m, v)
+        return pixelFromRGB(m, v, n);
+        //return long(n ) << 16 | long(m) << 8 | long(v);
+    case 5: // (v, m, n)
+        return pixelFromRGB(m, n, v);
+        //return long(v ) << 16 | long(m) << 8 | long(n);
+    }
+}
+
+
+
+void rainbow(strand_t * pStrand, unsigned long delay_ms, unsigned long timeout_ms)
+{
+  const uint8_t color_div = 4;
+  const uint8_t anim_step = 1;
+  const uint8_t anim_max = pStrand->brightLimit - anim_step;
+  pixelColor_t color1 = pixelFromRGB(anim_max, 0, 0);
+  pixelColor_t color2 = pixelFromRGB(anim_max, 0, 0);
+  uint8_t stepVal1 = 0;
+  uint8_t stepVal2 = 0;
+  bool runForever = (timeout_ms == 0 ? true : false);
+  unsigned long start_ms = millis();
+  while (runForever || (millis() - start_ms < timeout_ms)) {
+    color1 = color2;
+    stepVal1 = stepVal2;
+    for (uint16_t i = 0; i < pStrand->numPixels; i++) {
+      pStrand->pixels[i] = pixelFromRGB(color1.r/color_div, color1.g/color_div, color1.b/color_div);
+      if (i == 1) {
+        color2 = color1;
+        stepVal2 = stepVal1;
+      }
+      switch (stepVal1) {
+        case 0:
+        color1.g += anim_step;
+        if (color1.g >= anim_max)
+          stepVal1++;
+        break;
+        case 1:
+        color1.r -= anim_step;
+        if (color1.r == 0)
+          stepVal1++;
+        break;
+        case 2:
+        color1.b += anim_step;
+        if (color1.b >= anim_max)
+          stepVal1++;
+        break;
+        case 3:
+        color1.g -= anim_step;
+        if (color1.g == 0)
+          stepVal1++;
+        break;
+        case 4:
+        color1.r += anim_step;
+        if (color1.r >= anim_max)
+          stepVal1++;
+        break;
+        case 5:
+        color1.b -= anim_step;
+        if (color1.b == 0)
+          stepVal1 = 0;
+        break;
+      }
+    }
+    digitalLeds_updatePixels(pStrand);
+    vTaskDelay(delay_ms / portTICK_PERIOD_MS);
+  }
+  digitalLeds_resetPixels(pStrand);
+}
+
+
 /******************************************************************************/
 /*** Main Logic ***************************************************************/
 
@@ -338,6 +504,21 @@ void app_main(void)
 /* end display init */
 
     ESP_ERROR_CHECK( nvs_flash_init() );
+
+    // Initialise LEDs
+    LED_setup(LED_PIN, GPIO_MODE_OUTPUT, LOW);
+    if (digitalLeds_initStrands(STRANDS, STRANDCNT)) {
+        ESP_LOGE(TAG, "LED init failure :(");
+    }
+
+    // TEMP
+    while (true) {
+        for (int i = 0; i < STRANDCNT; i++) {
+            strand_t * pStrand = &STRANDS[i];
+            rainbow(pStrand, 10, 2000);
+            digitalLeds_resetPixels(pStrand);
+        }
+    }
 
     // Go for WiFi!
     initialise_wifi();
